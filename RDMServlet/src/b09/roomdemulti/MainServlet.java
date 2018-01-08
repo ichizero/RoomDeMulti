ab4481937c20101a4b45086e0006f8084f769f96
yuto2746 <31988171+yuto2746@users.noreply.github.com>
2018-01-08 12:30:12 +0900

    コメントアウトの修正と処理を分岐させるためのメソッドを追加

--- RDMServlet/src/b09/roomdemulti/MainServlet.java
+++ RDMServlet/src/b09/roomdemulti/MainServlet.java
@@ -12,7 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MainServlet
+ * サーブレット実装クラス
+ * 
+ * @author Yuto Murakami
  */
 @WebServlet("/test")
 public class MainServlet extends HttpServlet {
@@ -32,77 +34,15 @@ public class MainServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String func = request.getParameter("func");
 
 		StringBuilder builder = new StringBuilder();
 		builder.append('{');
 
-		// ユーザID
-		String userId = null;
-		// パスワード
-		String password = null;
-		// ユーザのマルチURL
-		String userURL = null;
-		// ルームID
-		String roomId = null;
-		// dbManager から受け取った文字列を格納
-		String buff = "";
+		// クライアントサイドからどのような処理を行うのかを受け取る
+		String func = request.getParameter("func");
+		// dbManager で処理を行い，受け取った結果を格納
+		String buff = this.branchProcessing(func, request);
 
-		// func によって処理を変える
-		switch(func) {
-		case "auth":
-			userId = request.getParameter("userId");
-			password = request.getParameter("password");
-			buff = this.login(userId, password);
-			builder.append(buff);
-			break;
-		case "regi":
-			userId = request.getParameter("userId");
-			password = request.getParameter("password");
-			userURL = request.getParameter("userURL");
-			buff = this.registerAccount(userId, password, userURL);
-			builder.append(buff);
-			break;
-		case "addRequest":
-			// userId = request.getParameter("userId");
-			// userURL = request.getParameter("userURL");
-			roomId = request.getParameter("roomId");
-			String requestMessage = request.getParameter("requestMessage");
-			buff = this.addRequest(roomId, requestMessage);
-			builder.append(buff);
-			// builder.append("\"userId\":\"").append(userId).append("\",");
-			// builder.append("\"userURL\":\"").append(userId).append("\",");
-			// builder.append("\"requestMessage\":\"").append(userURL).append("\"");
-			break;
-		case "getRequest":
-			roomId = request.getParameter("roomId");
-			buff = this.getRoomInf(roomId);
-			builder.append(buff);
-			break;
-		case "getRoomList":
-			userId = request.getParameter("userId");
-			buff = this.getRoomList(userId);
-			builder.append(buff);
-		case "createRoom":
-			roomId = request.getParameter("roomId");
-			userId = request.getParameter("userId");
-			buff = addRoom(roomId);
-			builder.append(buff);
-			// builder.append("\"roomId\":\"").append(roomId).append("\"");
-			break;
-		case "joinRoom":
-			roomId = request.getParameter("roomId");
-			userId = request.getParameter("userId");
-			buff = joinRoom(roomId);
-			builder.append(buff);
-			// builder.append("\"roomId\":\"").append(roomId).append("\"");
-			break;
-		default:
-			break;
-		}
-
-		// builder.append("\"integer\":\"").append(integer).append("\",");
-		// builder.append("\"digit\":\"").append(String.valueOf(digit)).append("\"");
 		builder.append('}');
 		String json = builder.toString();
 		System.out.println(json);
@@ -113,28 +53,76 @@ public class MainServlet extends HttpServlet {
 	}
 
 	/**
+	 * 引数に与えられた内容によって処理を分岐させ，処理した結果を返す
+	 */
+	protected String branchProcessing(String func, HttpServletRequest request) {
+		
+		// ユーザID
+		String userId = "";
+		// パスワード
+		String password = "";
+		// ユーザのマルチURL
+		String userURL = "";
+		// ルームID
+		String roomId = "";
+
+		// func によって処理を変える
+		switch(func) {
+		case "auth":
+			userId = request.getParameter("userId");
+			password = request.getParameter("password");
+			return this.login(userId, password);
+		case "regi":
+			userId = request.getParameter("userId");
+			password = request.getParameter("password");
+			userURL = request.getParameter("userURL");
+			return this.registerAccount(userId, password, userURL);
+		case "addRequest":
+			roomId = request.getParameter("roomId");
+			String requestMessage = request.getParameter("requestMessage");
+			return this.addRequest(roomId, requestMessage);
+		case "getRequest":
+			roomId = request.getParameter("roomId");
+			return this.getRoomInf(roomId);
+		case "getRoomList":
+			userId = request.getParameter("userId");
+			return this.getRoomList(userId);
+		case "createRoom":
+			roomId = request.getParameter("roomId");
+			userId = request.getParameter("userId");
+			return this.addRoom(roomId);
+		case "joinRoom":
+			roomId = request.getParameter("roomId");
+			userId = request.getParameter("userId");
+			return this.joinRoom(roomId);
+		default:
+			return "";
+		}
+	}
+
+	/**
 	 * ログインを行うメソッド
 	 *
-	 * @param id ユーザID
-	 * @param pass パスワード
-	 * @return ログインが行われていれば true を返す
+	 * @param userId ユーザID
+	 * @param password パスワード
+	 * @return ログインが正しく行われていれば，ユーザIDとユーザのマルチURLの JSON を返す
 	 */
-	protected String login(String id, String pass) {
-		return dbm.admit(id, pass);
+	protected String login(String userId, String password) {
+		return dbm.admit(userId, password);
 	}
 
 	/**
 	 * アカウントの作成を行うメソッド
 	 *
-	 * @param id ユーザID
-	 * @param pass パスワード
-	 * @param multiURL マルチURL
-	 * @return アカウントの登録ができれば true を返す
+	 * @param userId ユーザID
+	 * @param password パスワード
+	 * @param userURL ユーザのマルチURL
+	 * @return アカウントの登録ができれば，ユーザIDとユーザのマルチURLをJSON形式の文字列で返す
 	 */
-	protected String registerAccount(String id, String pass, String multiURL) {
+	protected String registerAccount(String userId, String password, String userURL) {
 		// 既に存在するIDでなければ，アカウントをデータベースに追加する
-		if (!dbm.isExistingID(id)) {
-			return addAccount(id, pass, multiURL);
+		if (!dbm.isExistingID(userId)) {
+			return addAccount(userId, password, userURL);
 		} else {
 			return "";
 		}
@@ -144,6 +132,7 @@ public class MainServlet extends HttpServlet {
 	 * ルーム一覧を表示するためのメソッド
 	 *
 	 * @param userId ユーザID
+	 * @return ユーザの所属しているルーム一覧をJSON形式の文字列で返す
 	 */
 	protected String getRoomList(String userId) {
 		return dbm.getRoomList(userId);
@@ -153,6 +142,7 @@ public class MainServlet extends HttpServlet {
 	 * ルームの追加を行うメソッド
 	 *
 	 * @param roomId ルームID
+	 * @return 正しく追加されれば，ルームIDをJSON形式の文字列で返す
 	 */
 	protected String addRoom(String roomId) {
 		return dbm.addRoom(roomId);
@@ -163,9 +153,10 @@ public class MainServlet extends HttpServlet {
 	 * (クエストの募集一覧の表示など)
 	 * 
 	 * @param roomId ルームID
+	 * @return 指定されたルームの募集文一覧をJSON形式の文字列で返す
 	 */
 	protected String getRoomInf(String roomId) {
-		return dbm.getRoomInf();
+		return dbm.getRoomInf(roomId);
 	}
 
 	/**
@@ -173,6 +164,7 @@ public class MainServlet extends HttpServlet {
 	 * ( Room クラスの userList にユーザを追加する)
 	 *
 	 * @param roomId ルームID
+	 * @return ルームに参加できれば，ルームIDをJSON形式の文字列で返す
 	 */
 	protected String joinRoom(String roomId) {
 		return dbm.joinRoom(roomId);
@@ -183,6 +175,7 @@ public class MainServlet extends HttpServlet {
 	 *
 	 * @param roomId ルームID
 	 * @param requestMessage 募集文
+	 * @return クエスト募集が正しく追加されれば，ルームの募集文一覧をJSON形式の文字列で返す
 	 */
 	protected String addRequest(String roomId, String requestMessage) {
 		return dbm.addRequest(requestMessage, roomId);
